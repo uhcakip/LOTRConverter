c2af4654eae2f0151a70893589bb7478d8777e9a
yunac <yunac.owob@gmail.com>
2024-08-06 13:35:03 +0800

    chore(ExchangeInfoView): lay out stacks and add structural comments

---
+++ LOTRConverter/ExchangeInfoView.swift
@@ -0,0 +1,37 @@
+//
+//  ExchangeInfoView.swift
+//  LOTRConverter
+//
+//  Created by Yuna Chou on 2024/8/6.
+//
+
+import SwiftUI
+
+struct ExchangeInfoView: View {
+    var body: some View {
+        ZStack {
+            // background image
+
+            VStack {
+                // title text
+
+                // description text
+
+                // exchange rates
+                HStack {
+                    // left currency image
+
+                    // exhange rate text
+
+                    // right currency image
+                }
+
+                // done button
+            }
+        }
+    }
+}
+
+#Preview {
+    ExchangeInfoView()
+}
