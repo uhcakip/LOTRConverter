71775f64247c2e2682c19f12784d2a8286a60d5e
yunac <yunac.owob@gmail.com>
2024-08-06 16:15:50 +0800

    feat(ExchangeInfoView): implement main exchange info interface

--- LOTRConverter/ExchangeInfoView.swift
+++ LOTRConverter/ExchangeInfoView.swift
@@ -10,23 +10,43 @@ import SwiftUI
 struct ExchangeInfoView: View {
     var body: some View {
         ZStack {
-            // background image
+            Image(.parchment)
+                .resizable()
+                .scaleEffect(1.1)
+                .ignoresSafeArea()
 
             VStack {
-                // title text
+                Text("Exchange Rates")
+                    .font(.largeTitle)
+                    .tracking(3)
 
-                // description text
+                Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
+                    .font(.title3)
+                    .padding()
 
-                // exchange rates
+
                 HStack {
-                    // left currency image
+                    Image(.goldPiece)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
 
-                    // exhange rate text
+                    Text("1 Gold Piece = 4 Gold Pennies")
 
-                    // right currency image
+                    Image(.goldPenny)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
                 }
 
-                // done button
+                Button("Done") {
+
+                }
+                .font(.title)
+                .buttonStyle(.borderedProminent)
+                .tint(.brown)
+                .padding()
+
             }
         }
     }
